ad0b05bc8b7babcad8d365e0b2a43eab1fab02d8
Yasin <yasinreisoglu@outlook.com>
2020-05-08 00:11:59 +0300

    Yasin
    Tutorial için css edit

--- js/apiConnector.js
+++ js/apiConnector.js
@@ -51,7 +51,6 @@ function getLevels() {
     xhrContent.open('GET', contentUrl + currentLevel, true);
     xhrContent.send();
     xhrContent.onload = () => {
-        console.log(xhrContent.responseText);
         contentJson = JSON.parse(xhrContent.responseText);
         courseDiv.innerHTML = contentJson.course + contentJson.tutorial;
         header.innerHTML = contentJson.header.toUpperCase();
@@ -63,20 +62,21 @@ function getLevels() {
 function compilerApi() {
     xhr.open('POST', proxy + myUrl, true);
     xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
     value = editor.getValue();
     var data = JSON.stringify({
-        "clientId": "b82ecbc8597c915d6457172b1a5e4726", 
-        "clientSecret": "4b23f21be5921030cc3d8339f56c7e1e80832675fa73406bc4ef429461e9b61a", 
-        "script": value, 
-        "language": "python3", 
-        "versionIndex": "0" 
+        "clientId": "b82ecbc8597c915d6457172b1a5e4726",
+        "clientSecret": "4b23f21be5921030cc3d8339f56c7e1e80832675fa73406bc4ef429461e9b61a",
+        "script": value,
+        "language": "python3",
+        "versionIndex": "0"
     });
     xhr.send(data);
     xhr.onload = () => {
         responseJson = JSON.parse(xhr.responseText);
         div.innerHTML = responseJson.output;
         var lowerRes = responseJson.output.toLowerCase();
-        if (lowerRes.includes(contentJson.answer)) {
+        if (value.includes(contentJson.code)) {
             console.log("Done.");
             levelStatus = true;
         } else {
